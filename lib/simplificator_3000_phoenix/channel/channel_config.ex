1a13e28ac8442ff60e5f8492dee81322d861ae18
Jan Rada <31016933+ZelvaMan@users.noreply.github.com>
2023-01-12 13:35:27 +0100

    delete unused config

--- lib/simplificator_3000_phoenix/channel/channel_config.ex
+++ lib/simplificator_3000_phoenix/channel/channel_config.ex
@@ -23,38 +23,6 @@ defmodule Simplificator3000Phoenix.Channel.ChannelConfig do
     end
   end
 
-  @fallback_handler_key :fallback_handler
-  def get_fallback_handler(options) do
-    with nil <- Keyword.get(options, @fallback_handler_key, nil),
-         nil <- get_from_config(@fallback_handler_key) do
-      &Channel.DefaultHandlers.default_fallback_controller/3
-    end
-  end
-
-  @fallback_enabled_key :fallback_enabled
-  def get_fallback_enabled(options) do
-    with nil <- Keyword.get(options, @fallback_enabled_key, nil),
-         nil <- get_from_config(@fallback_enabled_key) do
-      true
-    end
-  end
-
-  @permission_handler :permission_handler
-  def get_permission_handler(options) do
-    with nil <- Keyword.get(options, @permission_handler, nil),
-         nil <- get_from_config(@permission_handler) do
-      raise "permission handler not defined"
-    end
-  end
-
-  @auth_operator_key :auth_operator
-  def get_auth_operator(options) do
-    with nil <- Keyword.get(options, @auth_operator_key, nil),
-         nil <- get_from_config(@auth_operator_key) do
-      :or
-    end
-  end
-
   @unauthorized_handler_key :unauthorized_handler
   def get_unauthorized_handler(options) do
     with nil <- Keyword.get(options, @unauthorized_handler_key, nil),
