5a09ee0e688cc543e7e18c2ee1472dd4b58911c5
Filip Jakab <jakabfilip.fj@gmail.com>
2024-11-21 13:04:46 +0100

    v1.3.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Simplificator3000Phoenix.MixProject do
   def project do
     [
       app: :simplificator_3000_phoenix,
-      version: "1.2.4",
+      version: "1.3.0",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       name: "Simplificator3000 Phoenix",
